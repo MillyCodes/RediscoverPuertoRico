a3e32b1f0c48216b81b9b4956bec7f5de64201fc
Milly R <millz118@gmail.com>
2018-08-13 15:03:51 -0400

    Added routing in app for login and sign along with corresponding erb files.

--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
 require "sinatra"
 require "sinatra/activerecord"
+require "sinatra/flash"
 
-# require './models/judge.rb'
-# require './models/skater.rb'
-# require './models/score.rb'
+# require './models/user.rb'
+# require './models/post.rb'
+# require './models/tag.rb'
 
 # set :database, {adapter: 'postgresql', database: 'olympics'}
 # enable :sessions
@@ -33,6 +34,56 @@ end
 #Sign In Form Route
 get '/login' do 
     erb :login
+end
+
+# Sign in Route
+post '/login' do
+    user = User.find_by(username: params[:username])
+
+    #If the user exists, and pw correct, then create a session and redirect them to a logged in page
+    if user && user.password == params[:password]
+        #Create the cookie with the users id
+        session[:user_id] = user.id
+        #Print a helpful message
+        flash[:info] = "#{user.username} has logged in"
+        #redirect to homepage
+        redirect '/'
+    else 
+        flash[:warning] = "Your username does not exist or your password is not correct"
+        redirect '/login'
+    end
+end
+
+#Sign Up Form Route
+
+get '/signup' do 
+    erb :sign_up
+end
+
+#Sign Up Route
+
+post '/signup' do 
+    user = User.create(
+        username: params[:username],
+        password: params[:password],
+        name: params[:name],
+        email: params[:email],
+        b_day: params[:b_day]
+    )
+    session[:user_id] = user.id
+    redirect '/'
+end
+
+
+
+
+
+get '/' do
+    if session[:user_id] 
+        erb :homepage
+    else
+        erb :sign_in
+    end
 end
 
 
