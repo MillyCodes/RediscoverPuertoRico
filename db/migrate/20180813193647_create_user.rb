c7c867ba51ebb3388033e1c4f85ba6e8ef964561
Milly R <millz118@gmail.com>
2018-08-13 15:42:10 -0400

    Added routing in app for other routes and made sure erb files matched

---
+++ db/migrate/20180813193647_create_user.rb
@@ -0,0 +1,10 @@
+class CreateUser < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.string :name
+      t.string :email
+      t-string :b_day
+  end
+end
