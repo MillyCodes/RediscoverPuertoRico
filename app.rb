61f7b32757a5b887b4c2e3a42e4a86c77ebc2798
Milly R <millz118@gmail.com>
2018-08-14 10:30:14 -0400

    view fix

--- app.rb
+++ app.rb
@@ -1,94 +1,119 @@
 require "sinatra"
 require "sinatra/activerecord"
+require "sinatra/flash"
 
-# require './models/judge.rb'
-# require './models/skater.rb'
-# require './models/score.rb'
+require './models/user.rb'
+require './models/post.rb'
+require './models/tag.rb'
+require './models/post_tag.rb'
 
-# set :database, {adapter: 'postgresql', database: 'olympics'}
-# enable :sessions
+set :database, {adapter: 'postgresql', database: 'pr-blog'}
+enable :sessions
 
-# links to all other pages -works, update routes here
-# main index, shows all blog posts and links
+# links to all other pages -works, BUT UPDATE ROUTES!
+# main index, shows all blog posts and links, SEE 
+# IF ALL POST SHOW UP ON LOOP FOR INDEX.ERB FILE
 get '/' do
     erb :index
 end
 
-# get about page post
+
+# get '/' do
+#     if session[:user_id] 
+#         erb :homepage
+#     else
+#         erb :sign_in
+#     end
+# end
+
+
+# get about page post -route works/erb done
 get '/about' do
     erb :about
 end
 
-# get contact form/info post
+# get contact form/info post -route works/erb NOT done
 get '/contact' do
     erb :contact
 end
 
 
 # get single post by id when clicked from main page
-get '/single_post' do
+# TEST AFTER SEED
+get '/single_post/:id' do
+    @specific_post = Post.find(params[:id])
     erb :single_post
 end
 
-#Sign In Form Route
-get '/login' do 
-    erb :login
-end
-
-
-
-# Should show information about all skaters --works
-# Each skaters name should link to thier specific page at /skater/:id
-# get '/skaters' do
-#     @skaters = Skater.all
-#     erb :skaters
-# end
-
-# Should show information about all judges --works
-# Each judges name should link to thier specific page at /judge/:id
-# get '/judges' do
-#     @judges = Judge.all
-#     puts @judges
-#     erb :judges
-# end
-
-# --works This page should show all the skaters the judge has judged and they score the judge gave them
 # get '/judge/:id' do
 #     @specific_judge = Judge.find(params[:id])
 #     @judge_scores = @specific_judge.scores
 #     erb :specific_judge 
 # end
 
-# From here you should have a link to the edit page for this specific skater /skater/:id/edit
-# get '/skater/:id' do
-#     @specific_skater = Skater.find(params[:id])
-#     erb :specific_skater   
+#show all POSTS BY a specific USER
+get '/users/:id' do 
+    @specific_user = user.find(params[:id])
+    @owners_post = @specific_user.posts
+    erb :user_posts
+end
+
+# ====LOGIN STUFF====
+
+#Sign In Form Route
+get '/login' do 
+    erb :login
+end
+
+# Sign in Route
+post '/login' do
+    user = User.find_by(username: params[:username])
+
+    #If the user exists, and pw correct, then create a session and redirect them to a logged in page
+    if user && user.password == params[:password]
+        #Create the cookie with the users id
+        session[:user_id] = user.id
+        #Print a helpful message
+        flash[:info] = "#{user.username} has logged in"
+        #redirect to homepage
+        redirect '/'
+    else 
+        flash[:warning] = "Your username does not exist or your password is not correct"
+        redirect '/login'
+    end
+end
+
+#Sign Up Form Route
+
+get '/signup' do 
+    erb :sign_up
+end
+
+#Sign Up Route
+
+post '/signup' do 
+    user = User.create(
+        username: params[:username],
+        password: params[:password],
+        name: params[:name],
+        email: params[:email],
+        b_day: params[:b_day]
+    )
+    session[:user_id] = user.id
+    redirect '/'
+end
+
+
+# delete user or post?
+# delete '/dogs/:id' do 
+#     @current_dog = Dog.find(params[:id])
+#     @current_dog.destroy
+#     redirect '/dogs'
 # end
 
-# NEW This page should allow you to create a new skater and add them to the database and then redirect you to the all skaters page /skaters
-# get '/skaters/new' do
-#     erb :new_skater
+# private
+# private methods?
 
-# end
-
-# post '/skaters' do 
-#     Skater.create(name: params[:name], nationality: params[:nationality], age: params[:age], photo_url: params[:photo_url], is_favorite: params[:is_favorite], number_of_medals: params[:number_of_medals], average_score: params[:average_score])
-#     redirect '/skaters'
-# end
-
-# get '/skater/:id/edit' do
-#     @current_skater = Skater.find(params[:id])
-#     erb :edit_skater
-# end
-
-# Update Action -> PUT/PATCH /resource/:id
-# put '/skater/:id' do 
-#     @current_skater = Skater.find(params[:id])
-#     @current_skater.update(name: params[:name], nationality: params[:nationality], age: params[:age], photo_url: params[:photo_url], is_favorite: params[:is_favorite], number_of_medals: params[:number_of_medals], average_score: params[:average_score])
-# end
-
-# delete '/skater/:id' do 
-#     @current_skater = Skater.find(params[:id])
-#     @current_skater.destroy
-#     redirect '/skaters'
+# def get_current_user 
+#     User.find(session[:user_id])
 # end
