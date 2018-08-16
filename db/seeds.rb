a648decf35155ab7a1735e5cf9d2ed7d6f3e13f3
Milly R <millz118@gmail.com>
2018-08-16 15:06:07 -0400

    duh

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ Post.create(
     user_id: 1,
     title: "Spend the Afternoon at Palomino Island",
     content: "No need to leave the island to enjoy the best water activities Puerto Rico has to offer. Guests of El Conquistador Resort can hop on the private catamaran, go scuba diving, snorkel, sail, windsurf, or ride horseback across the pristine-white beach of Palamino Island.",
-    image: "/img/pr-oceanside.jpg",                                                 
+    image: "/img/pr-oceanside.jpg"                                                
     )
 
 
